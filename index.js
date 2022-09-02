54ce49dda61b545bd04285b1c52d7383d831eb5f
arybkin <arybkin@tmxpub.agiotg.com>
2022-09-02 22:39:48 +0300

    start proj

---
+++ index.js
@@ -0,0 +1,4 @@
+// ! Создать массив инстурментов. Для вывода меню инстурментов использовать @grammyjs/menu => MenuRange
+const { Bot } = require("grammy");
+const { Menu, MenuRange } = require("@grammyjs/menu");
+require('dotenv').config();
