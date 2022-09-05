0edc64c9a1aee82627926f6c1fc680972a5ad669
arybkin <arybkin@tmxpub.agiotg.com>
2022-09-05 23:46:18 +0300

    some table fix; add method TableInfo.getInstruments()

--- dataObj.js
+++ dataObj.js
@@ -1,1 +1,16 @@
 const XLSX = require("xlsx");
+
+function TableInfo() {
+  this.workbook = XLSX.readFile("data/dataTable.xlsx");
+  this.worksheet = this.workbook.Sheets.store;
+  this.jsonSheet = XLSX.utils.sheet_to_json(this.worksheet);
+  this.instruments = [];
+
+  this.getInstruments = function () {    
+        return this.jsonSheet.map(item=>item['Инструменты готовые к отправке']);
+  };
+}
+
+module.exports = {
+  TableInfo,
+};
--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 const { Bot } = require("grammy");
 const { Menu, MenuRange } = require("@grammyjs/menu");
 require('dotenv').config();
-let {mainMenu} = require('./keyabords')
+let {mainMenu} = require('./keyabords');
+let {TableInfo} = require('./dataObj');
 
 const token = process.env.BOT_TOKEN;
 const bot = new Bot(token);
 
+let tableInfo = new TableInfo();
 
-bot.command("start", async (ctx) => { 
+bot.command("start", async (ctx) => {
+  
+  let instrumentsArr = tableInfo.getInstruments();
+  console.log(instrumentsArr)
+
     await ctx.reply("Выберите действие, которое желаете совершить:", { reply_markup: mainMenu});
   });
 
